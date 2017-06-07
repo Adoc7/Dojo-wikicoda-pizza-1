516d74daacb1197700aad488fe9d90717e4fd0c4
PyromaX <jeux.pyromaxx@gmail.com>
2017-06-07 16:58:25 +0200

    code commenté

--- list.php
+++ list.php
@@ -5,3 +5,41 @@
  * Date: 07/06/17
  * Time: 15:30
  */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Pizza</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Source+Sans+Pro" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-default">
+
+        <img class="col-sm-4 col-sm-offset-4" src="./images/logo2.png" alt="">
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="/">Accueil</a></li>
+            <li><a href="./list.php">Liste commandes</a></li>
+        </ul>
+    </nav>
+    <div class="buche">
+    </div>
+</header>
+    <section class="pizza container">
+
+    </section>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script type="text/javascript" src="./js/script.js"></script>
+
+</body>
+</html>
--- newcommande.php
+++ newcommande.php
@@ -6,8 +6,9 @@
  * Time: 15:31
  */
 //recuperer les entrées du formulaire
-if(isset($_POST)){
-    var_dump($_POST);
+if(isset($_POST)){//pour vérifier si le tqbleau post est vide ou pas
+//    var_dump($_POST); dans l objectif de verifier si la variable post existait bien et son contenu
+    // on vient ci dessous recuperer l ensemble du contenu $post
     $nom=$_POST["nom"];
     $telephone=$_POST["telephone"];
     $adresse=$_POST["adresse"];
@@ -18,12 +19,20 @@ if(isset($_POST)){
     $labootstrap=$_POST["nb_pizza5"];
     $lhtmlauseo=$_POST["nb_pizza6"];
 }
+// ci dessous: on cree l onbjet pdo : notre utilitaire qui sert a communiquer avec la bdd
+// 3 attributs: le 1er pour le nom et adresse de la bdd 2e pour id et 3e pour password
 
 $bdd= new PDO('mysql:host=localhost;dbname=pizza','root', 'root');
+
+// on prepare la requete sql
+// pour lma date, on met NOW qui suffit directement d avoir la date du j par mysql
 
 $requete = $bdd->prepare('INSERT INTO commandes(date,nom,tel,adresse,fourframeworks,gitpushpizza,beautifulcss,jqueryui,labootstrap,lhtmlauseo)
                                 VALUES (NOW(), :nom, :tel, :adresse, :fourframeworks, :gitpushpizza, :beautifulcss, :jqueryui, :labootstrap, :lhtmlauseo)
                                 ');
+
+// on remplit la requete avec nos variables contenant les valeurs saisies par l utilisateur
+
 $requete->bindParam(':nom', $nom);
 $requete->bindParam(':tel', $telephone);
 $requete->bindParam(':adresse', $adresse);
@@ -34,6 +43,6 @@ $requete->bindParam(':jqueryui', $jqueryui);
 $requete->bindParam(':labootstrap', $labootstrap);
 $requete->bindParam(':lhtmlauseo', $lhtmlauseo);
 
-
+// on execute la requete
 
 $requete->execute();
